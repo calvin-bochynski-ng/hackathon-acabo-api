238d8eb121e5ac5144c15aa8d9e87d9ddbffa3ef
Calvin <calvinng1993@gmail.com>
2024-01-26 15:22:05 +0000

    merge develop

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ const cors = require("cors");
 const app = express();
 const axios = require("axios");
 const PORT = process.env.PORT || 8080;
-const SERVER_API_URL = process.env.API_URL;
+const SERVER_API_URL = process.env.SERVER_API_URL;
 const addressRoutes = require("./routes/addresses");
+const giftMessageRoutes = require("./routes/giftMessages");
 
 app.use(express.json());
 app.use(cors());
@@ -15,6 +16,7 @@ app.use((req, res, next) => {
 });
 
 app.use("/address", addressRoutes);
+app.use("/gift-message", giftMessageRoutes);
 
 app.listen(
   PORT,
